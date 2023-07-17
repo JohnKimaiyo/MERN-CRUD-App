a713abfdf3e175c6467d5e93151f8d5d4863cd38
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-07-17 08:40:47 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import PostList from "./PostList";
 import AddPost from "./AddPost";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EditPost from "./EditPost";
+import bootstrap from "../node_modules/bootstrap/dist/css"
 export default function App() {
   return (
     <div className="App">
-      <h1> MERN Stack CRUD Applciation</h1>
+      <h1 style = {{backgroundColor:'black',color:'white' ,padding:'10px'}}> MERN Stack CRUD Applciation</h1>
       <BrowserRouter>
         <Routes>
           <Route path="/" component={PostList} exact />
