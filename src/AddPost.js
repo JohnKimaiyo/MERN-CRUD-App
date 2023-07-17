c9d1c439f5198d2c74cca317b21ca5388b838873
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-07-17 11:10:39 +0300

    final commit

--- src/AddPost.js
+++ src/AddPost.js
@@ -1,21 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 export default function AddPost() {
+  const [title, settitle] = useState("");
+  const [imageurl, setimageurl] = useState("");
+  const [description, setdescription] = useState("");
+
+  function addpost() {
+    var post = {
+      title: title,
+      imageurl: imageurl,
+      description: description,
+    };
+    console.log(post);
+  }
   return (
     <div className="row justify-content-center">
       <div className="col-md-6">
         <div>
-          <input type="text" placeholder="title" className="form-control" />
-          <input type="text" placeholder="imageurl" className="form-control" />
+          <input
+            type="text"
+            placeholder="title"
+            className="form-control"
+            value={title}
+            onChange={(e) => {
+              settitle(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="imageurl"
+            className="form-control"
+            value={imageurl}
+            onChange={(e) => {
+              setimageurl(e.target.value);
+            }}
+          />
           <textarea
             cols="30"
             rows="10"
             placeholder="Description"
             className="form-control"
+            value={description}
+            onChange={(e) => {
+              setdescription(e.target.value);
+            }}
           />
         </div>
       </div>
-      <button className="btn btn-success float-right">Boostrap Button</button>
+      <button onClick={addpost} className="btn btn-success float-left">
+        Add Post
+      </button>
     </div>
   );
 }
