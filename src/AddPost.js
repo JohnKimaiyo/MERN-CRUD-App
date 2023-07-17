74c82f4e33cd9c1d521b342c54fcc6f9e876e27c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-07-17 09:17:41 +0300

    finalcommit

--- src/AddPost.js
+++ src/AddPost.js
@@ -1,7 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import Button from "react-bootstrap/Button";
 export default function AddPost() {
   return (
-    <div>AddPost</div>
-  )
+    <div className="row justify-content-center">
+      <div className="col-md-6">
+        <div>
+          <input type="text" placeholder="title" className="form-control" />
+          <input type="text" placeholder="imageurl" className="form-control" />
+          <textarea
+            cols="30"
+            rows="10"
+            placeholder="Description"
+            className="form-control"
+          />
+        </div>
+      </div>
+      <button className="btn btn-success float-right">Boostrap Button</button>
+    </div>
+  );
 }
--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import PostList from "./PostList";
 import AddPost from "./AddPost";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EditPost from "./EditPost";
-import bootstrap from "../node_modules/bootstrap/dist/css"
+
 export default function App() {
   return (
     <div className="App">
-      <h1 style = {{backgroundColor:'black',color:'white' ,padding:'10px'}}> MERN Stack CRUD Applciation</h1>
+      <h1 style={{ backgroundColor: "black", color: "white", padding: "10px" }}>
+        {" "}
+        MERN Stack CRUD Applciation
+      </h1>
       <BrowserRouter>
         <Routes>
           <Route path="/" component={PostList} exact />
